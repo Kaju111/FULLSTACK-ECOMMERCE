f681647030f8bb832fc7eb47f06fecc29a43727d
kaju111 <saikiakaju275@gmail.com>
2024-01-07 20:02:04 +0530

    frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import { lazy,Suspense } from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { lazy, Suspense } from "react"
 import Leader from "./components/leader"
 import Header from "./components/header"
 
-const Home = lazy(()=>import("./pages/home"))
-const Search = lazy(()=>import("./pages/search")) 
-const Cart = lazy(()=>import( "./pages/cart")) 
-
+const Home = lazy(() => import("./pages/home"))
+const Search = lazy(() => import("./pages/search"))
+const Cart = lazy(() => import("./pages/cart"))
+const Shipping = lazy(() => import("./pages/shipping"))
 
 // Admin Routes Importing
 
@@ -33,46 +33,54 @@ const App = () => {
   return (
     <Router>
       {/* Header */}
-      <Header/>
+      <Header />
 
-    <Suspense fallback={<Leader/>}>
-    <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/search" element={<Search/>}/>
-        <Route path="/cart" element={<Cart/>}/>
+      <Suspense fallback={<Leader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/cart" element={<Cart />} />
+
+
+          // Logged In User Routes
+          <Route>
+            <Route path="/shipping" element={<Shipping />} />
+          </Route>
+
+
 
       // Admin Routes
 
 
-<Route
-  // element={
-  //   <ProtectedRoute isAuthenticated={true} adminRoute={true} isAdmin={true} />
-  // }
->
-  <Route path="/admin/dashboard" element={<Dashboard />} />
-  <Route path="/admin/product" element={<Products />} />
-  <Route path="/admin/customer" element={<Customers />} />
-  <Route path="/admin/transaction" element={<Transaction />} />
-  {/* Charts */}
-  <Route path="/admin/chart/bar" element={<Barcharts />} />
-  <Route path="/admin/chart/pie" element={<Piecharts />} />
-  <Route path="/admin/chart/line" element={<Linecharts />} />
-  {/* Apps */}
-  <Route path="/admin/app/coupon" element={<Coupon />} />
-  <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
-  <Route path="/admin/app/toss" element={<Toss />} />
+          <Route
+          // element={
+          //   <ProtectedRoute isAuthenticated={true} adminRoute={true} isAdmin={true} />
+          // }
+          >
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/product" element={<Products />} />
+            <Route path="/admin/customer" element={<Customers />} />
+            <Route path="/admin/transaction" element={<Transaction />} />
+            {/* Charts */}
+            <Route path="/admin/chart/bar" element={<Barcharts />} />
+            <Route path="/admin/chart/pie" element={<Piecharts />} />
+            <Route path="/admin/chart/line" element={<Linecharts />} />
+            {/* Apps */}
+            <Route path="/admin/app/coupon" element={<Coupon />} />
+            <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
+            <Route path="/admin/app/toss" element={<Toss />} />
 
-  {/* Management */}
-  <Route path="/admin/product/new" element={<NewProduct />} />
+            {/* Management */}
+            <Route path="/admin/product/new" element={<NewProduct />} />
 
-  <Route path="/admin/product/:id" element={<ProductManagement />} />
+            <Route path="/admin/product/:id" element={<ProductManagement />} />
 
-  <Route path="/admin/transaction/:id" element={<TransactionManagement />} />
-</Route>;
+            <Route path="/admin/transaction/:id" element={<TransactionManagement />} />
+          </Route>;
 
 
-    </Routes>
-    </Suspense>
+        </Routes>
+      </Suspense>
 
 
     </Router>
--- frontend/src/pages/cart.tsx
+++ frontend/src/pages/cart.tsx
@@ -1,8 +1,89 @@
+import { useEffect, useState } from "react";
+import { VscError } from "react-icons/vsc"
+import CartItem from "../components/cartItem";
+import { Link } from "react-router-dom";
+
+const cartItem = [
+  {
+
+    productId: "aknlakoinqk",
+    photo: "https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/71TPda7cwUL._SX679_.jpg",
+    name: "MacBook",
+    price: 3000,
+    quantity: 40,
+    stock: 10
+  },
+];
+
+
+const subtotal = 4000;
+const tax = Math.round(subtotal * 0.18)
+const shippingCharges = 200;
+const discount = 400;
+const total = subtotal + tax + shippingCharges;
 
 const Cart = () => {
+
+  const [couponCode, setCouponCode] = useState<string>("")
+  const [isValidCouponCode, setIsValidCouponCode] = useState<boolean>(false)
+
+  useEffect(() => {
+
+    const timeOutID = setTimeout(() => {
+      if (Math.random() > 0.5) setIsValidCouponCode(true)
+      else setIsValidCouponCode(false)
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeOutID)
+      setIsValidCouponCode(false)
+    }
+  }, [])
+
+
+
   return (
-    <div>
-      
+    <div className="cart">
+      <main>
+
+        {cartItem.length > 0 ?
+          cartItem.map((i, idx) => (
+            <CartItem key={idx} cartItem={i} />
+          )) :
+          <h1>No Item Added</h1>
+        }
+
+
+      </main>
+      <aside>
+        <p>Subtotal: ₹{subtotal}</p>
+        <p>ShippingCharges: ₹{shippingCharges}</p>
+        <p>Tax: ₹{tax}</p>
+        <p>
+          Discount : <em className="red">
+            - ₹{discount}
+          </em>
+        </p>
+        <p><b>
+          Total: ₹{total}
+        </b></p>
+        <input type="text" value={couponCode} placeholder="Coupon Code" onChange={(e) => setCouponCode(e.target.value)} />
+
+        {
+          couponCode && (
+            (isValidCouponCode ?
+              <span className="green"> ₹{discount} off using the <code>{couponCode}</code></span>
+              :
+              <span className="red">Invalid Coupon <VscError /></span>)
+          )
+        }
+
+        {
+          cartItem.length > 0 && <Link to="/shipping">Checkout</Link>
+        }
+
+
+      </aside>
     </div>
   )
 }
