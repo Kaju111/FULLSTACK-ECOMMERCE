b4807a685ac0539d167c65266cc52a89076dda39
kaju111 <saikiakaju275@gmail.com>
2024-01-08 21:06:39 +0530

    frontend

---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react"
+import { FcGoogle } from 'react-icons/fc'
+
+const Login = () => {
+
+    const [gender, setGender] = useState("")
+    const [data, setData] = useState("")
+
+    return (
+        <div className="login">
+            <main>
+                <h1 className="heading">
+                    Login
+                </h1>
+
+                <div>
+                    <label>Gender</label>
+                    <select value={gender} onChange={e => setGender(e.target.value)}>
+                        <option value="">Select Gender</option>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                    </select>
+                </div>
+
+
+
+                <div>
+                    <label>Date of birth</label>
+                    <input type="data"
+                        value={data}
+                        onChange={(e) => setData(e.target.value)}
+
+                    />
+                </div>
+
+                <div>
+                    <p>Already Signed In Once</p>
+                    <button>
+                        <FcGoogle /> <span>Sign in with Google</span>
+                    </button>
+                </div>
+
+
+            </main>
+        </div>
+    )
+}
+
+export default Login
