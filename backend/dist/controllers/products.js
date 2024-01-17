47342d09e418d554d80f4ab5ea7162364e2f2996
kaju111 <saikiakaju275@gmail.com>
2024-01-17 20:52:15 +0530

    backend

--- backend/dist/controllers/products.js
+++ backend/dist/controllers/products.js
@@ -39,3 +39,66 @@ export const getAllCategories = TryCatch(async (req, res, next) => {
         categories,
     });
 });
+export const getAdminProducts = TryCatch(async (req, res, next) => {
+    const products = await Product.find({});
+    return res.status(200).json({
+        success: true,
+        products,
+    });
+});
+export const getSingleProduct = TryCatch(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product)
+        return next(new ErrorHandler("Product not found", 404));
+    return res.status(200).json({
+        success: true,
+        product,
+    });
+});
+export const updateProduct = TryCatch(async (req, res, next) => {
+    const { id } = req.params;
+    const { name, price, category, stock } = req.body;
+    const photo = req.file;
+    const product = await Product.findById(id);
+    if (!product)
+        return next(new ErrorHandler("Product not found", 404));
+    if (photo) {
+        rm(product.photo, () => {
+            console.log("Old photo Deleted");
+        });
+        product.photo = photo.path;
+    }
+    if (name)
+        product.name = name;
+    if (price)
+        product.price = price;
+    if (stock)
+        product.stock = stock;
+    if (category)
+        product.category = category;
+    await product.save();
+    return res.status(200).json({
+        success: true,
+        message: "Product Updated Successfully"
+    });
+});
+export const deleteProduct = TryCatch(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product)
+        return next(new ErrorHandler("Product not found", 404));
+    rm(product.photo, () => {
+        console.log("Product photo deleted");
+    });
+    await Product.deleteOne();
+    return res.status(200).json({
+        success: true,
+        message: "Product deleted Successfully"
+    });
+});
+export const getAllProducts = TryCatch(async (req, res, next) => {
+    const products = await Product.find({}).sort({ createdAt: -1 }).limit(5);
+    return res.status(200).json({
+        success: true,
+        products,
+    });
+});
